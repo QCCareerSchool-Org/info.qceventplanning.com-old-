470428bd7248ab0f04cab69ce1e9726bfd6af090
Dave Welsh <dave@qccareerschool.com>
2019-10-09 12:31:46 -0400

    Updates manifest and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,10 @@ module.exports = {
         name: `QC Event School`,
         short_name: `QC Event`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
