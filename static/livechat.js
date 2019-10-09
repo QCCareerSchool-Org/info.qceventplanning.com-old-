e03fee0a3dc3530f36fa8b51e19accbf93785303
Dave Welsh <dave@qccareerschool.com>
2019-10-08 21:09:46 -0400

    Updated group to 1

--- static/livechat.js
+++ static/livechat.js
@@ -3,7 +3,7 @@ window.__lc = window.__lc || {};
 window.__lc.license = 1056788;
 window.__lc.chat_between_groups = false;
 window.__lc.ga_version = 'gtag';
-window.__lc.group = 14;
+window.__lc.group = 1;
 (function () {
   var lc = document.createElement('script');
   lc.type = 'text/javascript';
