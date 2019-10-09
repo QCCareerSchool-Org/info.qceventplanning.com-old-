e29122153c56c9f02f352ba6d2e16afc223a19be
Dave Welsh <dave@qccareerschool.com>
2019-10-09 12:04:35 -0400

    Updates column sizes on testimonials

--- src/pages/free-event-course-catalog.tsx
+++ src/pages/free-event-course-catalog.tsx
@@ -96,10 +96,10 @@ const FreeEventCourseCatalog: React.FC = () => {
           <Slider dots={true} infinite={true} speed={500} adaptiveHeight={true} className="px-3 px-md-5 mx-4">
             <div>
               <Row className="align-items-center">
-                <Col xs="12" md="4" lg="3" className="text-center offset-lg-1">
+                <Col xs="12" md="4" lg="3" className="text-center">
                   <Img fixed={images.sian.childImageSharp.fixed} alt="Sian Gerryts" />
                 </Col>
-                <Col xs="12" md="8" lg="6">
+                <Col xs="12" md="8" lg="9">
                   <p className="lead">“The Event and Wedding Planning course has given me the skills I need to pursue my dream career. It feels amazing to be doing something I love.”</p>
                   <p className="lead text-right">—Sian Gerryts</p>
                 </Col>
@@ -107,10 +107,10 @@ const FreeEventCourseCatalog: React.FC = () => {
             </div>
             <div>
               <Row className="align-items-center">
-                <Col xs="12" md="4" lg="3" className="text-center offset-lg-1">
+                <Col xs="12" md="4" lg="3" className="text-center">
                   <Img fixed={images.michelle.childImageSharp.fixed} alt="Michelle Panton" />
                 </Col>
-                <Col xs="12" md="8" lg="6">
+                <Col xs="12" md="8" lg="9">
                   <p className="lead">“The course has been invaluable to my career in the wedding and event planning industry. It gave me the confidence to start my business and also gave me the support of the tutors when needed.”</p>
                   <p className="lead text-right">—Michelle Panton, Belle Weddings and Events</p>
                 </Col>
@@ -118,10 +118,10 @@ const FreeEventCourseCatalog: React.FC = () => {
             </div>
             <div>
               <Row className="align-items-center">
-                <Col xs="12" md="4" lg="3" className="text-center offset-lg-1">
+                <Col xs="12" md="4" lg="3" className="text-center">
                   <Img fixed={images.lindsay.childImageSharp.fixed} alt="Lindsay Vine" />
                 </Col>
-                <Col xs="12" md="8" lg="6">
+                <Col xs="12" md="8" lg="9">
                   <p className="lead">“I have loved every minute of the course. It’s in depth, informative, and the assignments are fun. I felt like I was working for an actual client, and was well prepared for the industry and starting my own business. I’m confident that my training will lead my company, The Grape Vine Events, to great success.”</p>
                   <p className="lead text-right">—Lindsay Vine, The Grape Vine Events</p>
                 </Col>
